b1f0f6db52ffc9f85e225d405eff61ce7b5dee1a
Felix <92216380+Felix200260@users.noreply.github.com>
2024-04-26 11:47:04 +0500

    агружаем документ. отвечает на вопросы по документу

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+
 # Доступ к API-ключу
 # Запрос API ключа если он не задан в окружении
 if 'OPENAI_API_KEY' not in os.environ:
     os.environ['OPENAI_API_KEY'] = getpass.getpass('OpenAI API Key: ')
 
 api_key = os.getenv("OPENAI_API_KEY")
-# api_key = os.getenv("OPENAI_API_KEY")
 
 model = ChatOpenAI(model="gpt-3.5-turbo")
 
 # Создание шаблона запроса
-prompt = ChatPromptTemplate.from_template("tell me a short joke about {topic}")
+prompt = ChatPromptTemplate.from_template("Ответь на вопрос: {question}")
 output_parser = StrOutputParser() # Класс для преобразования ответов модели в строки
 
 chain = prompt | model | output_parser
@@ -29,23 +32,43 @@ chain = prompt | model | output_parser
 # output_parser - ответ в строку
 # chain - ответ(цепочка обработки)
 
-# ---------------------------Код проверки работы модели------------------------------------
-# Выполнение запроса
-# try:
-#     result = chain.invoke({"topic": "машина"})
-#     print("Result:", result)
-# except Exception as e:
-#     print("An error occurred:", str(e))
-# ---------------------------Код проверки работы модели------------------------------------
 # loader = PyPDFLoader("C:/Users/felix/YandexDisk-korchevskyfelix\Programming/Programming/Python/GOSTRighter/pdf")
 loader = PyPDFLoader(r"C:/Users/felix/YandexDisk-korchevskyfelix\Programming/Programming/Python/GOSTRighter/pdf/7.32-2017.pdf")
 pages = loader.load_and_split()
 # print(pages[0])
 
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size=500,
+    chunk_overlap=0,
+    length_function=len,
+    is_separator_regex=False,
+)
 
-faiss_index = FAISS.from_documents(pages, OpenAIEmbeddings())
-docs = faiss_index.similarity_search("Какой шрифт следует использовать?", k=2)
-for doc in docs:
-    print(str(doc.metadata["page"]) + ":", doc.page_content[:300])
+# Разделяем текст документа на чанки
+chunks = text_splitter.split_documents(pages)
+print(f'Колличество чанков: {len(chunks)}')
+
+# faiss_index = FAISS.from_documents(pages, OpenAIEmbeddings())
+
+# Создаем индекс для частей документа
+faiss_index = FAISS.from_documents(chunks, OpenAIEmbeddings(api_key=api_key))
+
+while True:
+    user_input = input("Задайте вопрос или введите 'exit' для выхода: ")
+    if user_input.lower() == 'exit':
+        break
+    
+    # Поиск в документе с помощью векторного индекса
+    docs = faiss_index.similarity_search(user_input, k=1)  # k=1 возвращает наиболее релевантный фрагмент
+    if docs:
+        print("---Ответ из документа:", '\n', docs[0].page_content[:300])  # Выводит первые 300 символов наиболее релевантного ответа
+    else:
+        print("Информация по вашему запросу в документе не найдена.")
+
+
+
+
+
 
 
