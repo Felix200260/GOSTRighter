b05da012a035cdaf3de7b97964ca75003427364e
Felix <92216380+Felix200260@users.noreply.github.com>
2024-05-11 21:28:12 +0500

    модифицировал file_utils.py добалвена вывод не номер модифицированного файла , а время и дата его

--- file_utils.py
+++ file_utils.py
@@ -1,17 +1,13 @@
 import os  # Импорт модуля для работы с файлами
 from datetime import datetime  # Импорт модуля для работы с датой и временем
-import random  # Импорт модуля для генерации случайных чисел
-
 
 def generate_unique_filename(path):
     base_path, ext = os.path.splitext(path)  # Разбиваем путь на базовую часть и расширение
-    date_str = datetime.now().strftime("%d.%m.%Y")  # Формируем строку с датой в формате "дд.мм.гггг"
-    random_number = random.randint(10, 99)  # Генерируем случайное число в диапазоне от 10 до 99
-    modified_path = f"{base_path}_modified_{random_number}_{date_str}{ext}"  # Формируем уникальное имя файла
+    timestamp = datetime.now().strftime("%H-%M-%S_%d.%m.%Y")  # Формируем строку с временем и датой в формате "чч-мм-сс_дд.мм.гггг"
+    modified_path = f"{base_path}_modified_{timestamp}{ext}"  # Формируем уникальное имя файла с временной меткой
 
     while os.path.exists(modified_path):  # Если файл с таким именем уже существует
-        random_number = random.randint(10, 99)  # Генерируем другое случайное число
-        modified_path = f"{base_path}_modified_{random_number}_{date_str}{ext}"  # Формируем уникальное имя файла
+        timestamp = datetime.now().strftime("%H-%M-%S_%d.%m.%Y")  # Обновляем временную метку
+        modified_path = f"{base_path}_modified_{timestamp}{ext}"  # Формируем новое уникальное имя файла
 
     return modified_path  # Возвращаем уникальное имя файла
-
